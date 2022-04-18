86e7194bac30825a2eeea5b932223eb74bb34318
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-04-18 21:09:41 +0300

    Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            int index = 0;
+            char[] process = new char[]{'\\', '|', '/'};
+            while (!Thread.currentThread().isInterrupted()) {
+                if (index == process.length) {
+                    index = 0;
+                }
+                System.out.print("\r load: " + process[index]);
+                index++;
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
