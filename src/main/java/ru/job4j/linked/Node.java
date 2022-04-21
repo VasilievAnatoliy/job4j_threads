be1795471c4bdf4cb73849c0ec7f97af388e6813
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-04-21 21:51:17 +0300

    Immutable объекты

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,25 @@
+package ru.job4j.linked;
+
+/**
+ * Immutable
+ * 1. Все поля отмечены final.
+ * 2. Состояние объекта не изменяется после создания объекта.
+ * @param <T>
+ */
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
