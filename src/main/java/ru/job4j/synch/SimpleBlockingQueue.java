8ecdbfb4d72ced020b5b16a0ce0bf7631896a352
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-04-30 14:32:17 +0300

    Реализовать шаблон Producer Consumer

--- src/main/java/ru/job4j/synch/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/synch/SimpleBlockingQueue.java
@@ -35,7 +35,7 @@ public class SimpleBlockingQueue<T> {
      */
     public synchronized void offer(T value) throws InterruptedException {
         while (queue.size() == capacity) {
-               wait();
+            wait();
         }
         queue.offer(value);
         notifyAll();
