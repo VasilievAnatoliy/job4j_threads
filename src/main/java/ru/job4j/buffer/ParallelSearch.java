464bceaec3ce5a17b4ef5c8cfbddd7a4e44114bd
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-04-30 15:21:58 +0300

    Обеспечить остановку потребителя

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.buffer;
+
+import ru.job4j.synch.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index < 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                         try {
+                             Thread.sleep(500);
+                         } catch (InterruptedException e) {
+                             e.printStackTrace();
+                             Thread.currentThread().interrupt();
+                         }
+                    }
+                     consumer.interrupt();
+                }
+        ).start();
+    }
+}
